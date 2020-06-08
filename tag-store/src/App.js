7a7723fe8972714c284a265404c92193d6a9cc53
Arsentii Zakharchenko <arsentii.zakharchenko@gmail.com>
2020-06-08 13:03:17 +1000

    create login page

--- tag-store/src/App.js
+++ tag-store/src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Layout } from 'antd';
+import Login from './Login';
+import 'antd/dist/antd.css';
+
+const { Header, Footer, Content } = Layout;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Login />
+      {/* <Header>Header</Header>
+      <Content>Content</Content>
+      <Footer>Footer</Footer> */}
     </div>
   );
 }
